39c37299ee3b05b530e8be8b0385434271f02009
hdzubiria <hdzubiria@hotmail.com>
2021-02-27 19:44:10 -0500

    Error de nombre d estorage corregido

--- crear-infraestructura/main.tf
+++ crear-infraestructura/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_resource_group" "rg" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
 resource "azurerm_storage_account" "stAccount" {
-    name                     = "hdz_staccountcp2" 
+    name                     = "hdzstaccountcp2" 
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
