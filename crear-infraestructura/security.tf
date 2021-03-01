d30ff0f69bfae18edc25cc3b83fb9b40d30aa380
hdzubiria <hdzubiria@hotmail.com>
2021-03-01 08:41:08 -0500

    UN grupo de segurida por cada una de las maquinas

--- crear-infraestructura/security.tf
+++ crear-infraestructura/security.tf
@@ -33,7 +33,7 @@ resource "azurerm_network_interface_security_group_association" "k8s_mater_SecGr
 }
 
 
-resource "azurerm_network_security_group" "hdz_k8s_Worker1_SecGroup" {
+resource "azurerm_network_security_group" "hdz_k8s_worker1_SecGroup" {
     name                = "sshtraffic_worker1"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
@@ -55,26 +55,66 @@ resource "azurerm_network_security_group" "hdz_k8s_Worker1_SecGroup" {
     }
 }
 
-
 resource "azurerm_network_interface_security_group_association" "k8s_worker1_SecGroupAssociation" {
     network_interface_id      = azurerm_network_interface.nic_k8s_worker1.id
-    network_security_group_id = azurerm_network_security_group.hdz_k8s_Worker1_SecGroup.id
+    network_security_group_id = azurerm_network_security_group.hdz_k8s_worker1_SecGroup.id
 
 }
 
 
+resource "azurerm_network_security_group" "hdz_k8s_worker2_SecGroup" {
+    name                = "sshtraffic_worker2"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
 
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 
-
+    tags = {
+        environment = "CP2"
+    }
+}
 
 resource "azurerm_network_interface_security_group_association" "k8s_worker2_SecGroupAssociation" {
     network_interface_id      = azurerm_network_interface.nic_k8s_worker2.id
-    network_security_group_id = azurerm_network_security_group.hdz_k8s_SecGroup.id
+    network_security_group_id = azurerm_network_security_group.hdz_k8s_worker2_SecGroup.id
 
+}
+
+
+resource "azurerm_network_security_group" "hdz_k8s_nfs_SecGroup" {
+    name                = "sshtraffic_nfs"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "CP2"
+    }
 }
 
 resource "azurerm_network_interface_security_group_association" "k8s_nfs_SecGroupAssociation" {
     network_interface_id      = azurerm_network_interface.nic_k8s_nfs.id
-    network_security_group_id = azurerm_network_security_group.hdz_k8s_SecGroup.id
+    network_security_group_id = azurerm_network_security_group.hdz_k8s_nfs_SecGroup.id
 
 }
